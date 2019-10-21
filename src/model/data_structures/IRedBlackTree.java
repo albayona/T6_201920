99c4979e6d02c68395a397d2f746f070597e59c8
Andres Bayona <al.bayona@uniandes.edu.co>
2019-10-21 18:49:48 -0500

    Changes

--- src/model/data_structures/IRedBlackTree.java
+++ src/model/data_structures/IRedBlackTree.java
@@ -1,4 +1,20 @@
 package model.data_structures;
 
-public interface IRedBlackTree {
+import java.util.Iterator;
+
+public interface IRedBlackTree <K extends Comparable<K>, V> {
+
+    int size();
+    boolean isEmpty ();
+    V get(K key);
+    int getHeight(K key);
+    boolean contains(K key);
+    void put(K key, V val);
+    int height();
+    K min();
+    K max();
+    boolean check();
+    Iterator <K> keys();
+    Iterator<V> valuesInRange(K init, K end);
+    Iterator<K> keysInRange(K init, K end);
 }
